fcb8830bff7f83cfbfb4ccb0b86646f9fa8e51d5
shekhar-patil <patilshekhar900@gmail.com>
2020-04-18 22:54:18 +0530

    Remove unnecessary changes

--- src/api/git.js
+++ src/api/git.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://api.github.com',
-  auth: {
-    username: 'shekhar-amura',
-    password: '2769ea6e5ea9e0fd6d045756798cfa1f56b1bdba'
+  headers: {
+    Authorization: 'token your token'
   }
 });
