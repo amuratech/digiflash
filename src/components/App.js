e2f90222d33770b7504881dea158183c2d232f1e
pooja-amura <pooja.k@amuratech.com>
2020-04-26 17:47:17 +0530

    Fixed footer issue

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Banner from './Banner';
 import Header from './Header';
 import Footer from './Footer';
 import PrTabs from './PrTabs';
+import './App.css';
 import { Container } from 'react-bootstrap';
 
 
